d8c462e3c759ffb763a81569a08c7b4610a11b67
Krzysztof Jura <kisioj@gmail.com>
2020-04-03 17:05:49 +0200

    added --update flag

--- dubbing_generator/main.py
+++ dubbing_generator/main.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 import sys
+from glob import glob
 
 from google.cloud import texttospeech
 from gtts import gTTS
@@ -34,7 +35,13 @@ def main():
         type=lambda x: does_file_exist(parser, x),
         metavar="FILE"
     )
-    parser.add_argument('--lang', '-l', default='pl', help='dubbing language (default=pl)')
+
+    parser.add_argument(
+        '--lang', '-l',
+        default='pl',
+        help='dubbing language (default=pl)'
+    )
+
     parser.add_argument(
         '--gender', '-s',
         default='male',
@@ -42,13 +49,26 @@ def main():
         choices=GENDERS,
         help='voice gender (default=male)'
     )
+
     parser.add_argument(
         '--service-key', '-k',
         help='path to google cloud service key (.json)',
         type=lambda x: does_file_exist(parser, x),
         metavar="KEY_FILE"
     )
-    parser.add_argument('--gui', '-g', action='store_true', help='run gui mode')
+
+    parser.add_argument(
+        '--gui',
+        '-g',
+        action='store_true',
+        help='run gui mode'
+    )
+
+    parser.add_argument(
+        '--update', '-u',
+        action='store_true',
+        help='do not create already existing wavs'
+    )
 
     args = parser.parse_args()
     gender = GENDERS[args.gender]
@@ -68,25 +88,49 @@ def main():
             gui.run()
             return
         else:
-            print("Google cloud service key is required to use GUI mode!", file=sys.stderr)
+            print("Google cloud service key is required to use GUI mode!",
+                  file=sys.stderr)
 
     ai_outputs = []
+    wavs_to_generate = set()
     for npc in data:
         for ai_output in npc['ai_outputs']:
-            ai_outputs.append((ai_output['wav_filename'], ai_output['text'], npc))
+            ai_outputs.append(
+                (ai_output['wav_filename'], ai_output['text'], npc)
+            )
+            wavs_to_generate.add(ai_output['wav_filename'])
+
+    if args.update:
+        wavs_generated = set(os.path.basename(path)[:-4] for path in glob(f"output/*.wav"))
+        wavs_generated &= wavs_to_generate
+        print(f'Found already generated {len(wavs_generated)} files.')
+    else:
+        wavs_generated = set()
+
+    ai_outputs_len = len(ai_outputs)
 
     if args.service_key:
+        print('Generating Advanced Dialogues')
         for i, ai_output in enumerate(ai_outputs):
             sound_filename, text, npc = ai_output
+            if sound_filename in wavs_generated:
+                print(f'\r{i}/{ai_outputs_len} {sound_filename}.wav - ALREADY EXISTS')
+                continue
+
             sound_path = os.path.join(OUTPUT_DIR, sound_filename)
             generate_advanced_voice(sound_path, text, npc, args.lang, gender)
-            print(f'\r{i}/{len(ai_outputs)} {sound_filename}.wav')
+            print(f'\r{i}/{ai_outputs_len} {sound_filename}.wav')
     else:
+        print('Generating Basic Dialogues (NO API KEY GIVEN)')
         for i, ai_output, _ in enumerate(ai_outputs):
             sound_filename, text = ai_output
+            if sound_filename in wavs_generated:
+                print(f'\r{i}/{ai_outputs_len} {sound_filename}.wav - ALREADY EXISTS')
+                continue
+
             sound_path = os.path.join(OUTPUT_DIR, sound_filename)
             generate_simple_voice(sound_path, text, args.lang, gender)
-            print(f'\r{i}/{len(ai_outputs)} {sound_filename}.wav')
+            print(f'\r{i}/{ai_outputs_len} {sound_filename}.wav')
 
 
 def generate_simple_voice(sound_path, text, lang, gender):
@@ -100,37 +144,11 @@ def generate_simple_voice(sound_path, text, lang, gender):
     os.remove(mp3_path)
 
 
-'''
-# from gui.gui import create_sound
-
-
-def create_sound(client, wav_path, text, language, voice, pitch, speed):
-    voice = texttospeech.types.VoiceSelectionParams(
-        language_code=language,
-        name=voice,
-    )
-
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.LINEAR16,
-        pitch=pitch,
-        speaking_rate=speed,
-    )
-
-    synthesis_input = texttospeech.types.SynthesisInput(text=text)
-    response = client.synthesize_speech(synthesis_input, voice, audio_config)
-
-    with open(wav_path, 'wb') as out:
-        out.write(response.audio_content)
-
-'''
-
-
 def generate_advanced_voice(sound_path, text, npc, lang, gender):
     language = npc.get('language', lang)
     voice_name = npc.get('voice')
     pitch = npc.get('pitch', 0.0)
     speed = npc.get('speed', 1.0)
-
 
     wav_path = sound_path + '.wav'
 
